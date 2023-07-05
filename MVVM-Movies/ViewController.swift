8047278c3dc8975102cd836da068d5e0bacf984a
mert deniz akbaş <meydey.md@gmail.com>
2023-07-05 16:11:34 +0300

    Initial Commit

---
+++ MVVM-Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Movies
+//
+//  Created by Mert Deniz Akbaş on 5.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
